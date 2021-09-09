cbf4f67bd4c08742c3acf76e2aaa6398fc120752
malinbragde <malin.forsgren@icloud.com>
2021-09-09 17:16:16 +0200

    Forgot these ones...

---
+++ src/components/Background.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Background() {
+    return (
+        <div className="curve">HÄR SKA DE VA EN KURVA</div>
+    )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h1>Hello, I'm footer!</h1>
+            
+        </div>
+    );
+}
