e0dcd7cf1fbfb98801b61f9eaf79cb0c0e09092d
Stella <biliou.stella@gmail.com>
2021-09-10 15:41:46 +0200

    Removed unused import in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Route} from "react-router-dom"
 import AllPosts from "./components/AllPosts.js";
 import OnePost from "./components/Post.js";
-import DefaultLayout from './components/layouts/DefaultLayout';
 //import Footer from "./components/Footer.js";
 //import Background from "./components/Background.js";
 import './styles/style.css';
