34fe818438b5b3e1a42f5955593b0797b023ad82
Stella <biliou.stella@gmail.com>
2021-09-17 22:58:16 +0800

    Removed unused import

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
-import BlockContent from "@sanity/block-content-to-react";
 import DefaultLayout from "./layouts/DefaultLayout.js";
 import { Link } from "react-router-dom";
 import '../styles/style.scss';
