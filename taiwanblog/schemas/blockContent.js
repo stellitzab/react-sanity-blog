129f2268cde3d810205900d83b076df7ec5d52c0
malinbragde <malin.forsgren@icloud.com>
2021-09-17 12:49:25 +0200

    Styling in blog-post: removed responive margin and styled ul-elements.

--- taiwanblog/schemas/blockContent.js
+++ taiwanblog/schemas/blockContent.js
@@ -36,6 +36,11 @@ export default {
         decorators: [
           {title: 'Strong', value: 'strong'},
           {title: 'Emphasis', value: 'em'},
+          {title: "Underline", "value": "underline" },
+          {title: "Strike", value: "strike-through" },  
+          {title: 'Code', value: 'code' },
+          
+
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
